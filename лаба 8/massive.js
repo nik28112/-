96ae5adfb0cafcb3c206839caaef6902035b0774
nik28112 <113481882+nik28112@users.noreply.github.com>
2022-12-24 20:07:04 +0300

    Add files via upload

---
+++ лаба 8/massive.js
@@ -0,0 +1,44 @@
+let data = [
+	{
+		"title" : "Работа с Git в Visual Studio Code",
+		"intro" : "Один из способов работать с Гитом.",
+		"labels" : ["Git", "Консоль"],
+		"image" : "/img/image.png",
+        "author": "@Fear",
+	},
+	{
+		"title" : "Adobe покупают Фигму. Всё пропало?",
+		"intro" : "Не факт, но есть разные мнения.",
+		"labels" : ["Figma"],
+		"image" : "/img/image 1.png",
+        "author": "@Dear",
+	},
+	{
+		"title" : "Все научились программировать. А дальше-то что?",
+		"intro" : "Ученье — свет.",
+		"labels" : ["Обучение", "Карьера"],
+		"image" : "/img/image 1-1.png",
+        "author": "@Lear",
+	},
+	{
+		"title" : "Мне тяжело учиться, кто поможет?",
+		"intro" : "Попали в патовую ситуацию? Тогда кликайте на статью.",
+		"labels" : ["Обучение"],
+		"image" : "/img/image 1-2.png",
+        "author": "@Sir",
+	},
+	{
+		"title" : "Ключевые навыки фронтендеров в 2022",
+		"intro" : "Ежегодное исследование рынка от HTML Academy.",
+		"labels" : ["Обучение", "Карьера"],
+		"image" : "/img/image 1-3.png",
+        "author": "@UserKonst",
+	},
+	{
+		"title" : "Фронтенд-новости",
+		"intro" : "WAI-ARIA, новинки HTML-форм и 12 ошибок любого джаваскриптера.",
+		"labels" : ["Новости"],
+		"image" : "/img/image 1-4.png",
+        "author": "@Anonymus",
+	},
+]
